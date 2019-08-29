302f0655e5ca58f79cf557d1b6d1b68cf99a076f
PikarryPham <phamngocthuytrang1026@gmail.com>
2019-08-29 21:42:51 +0700

    upload final

--- javascript/bounce.js
+++ javascript/bounce.js
@@ -1,1 +1,22 @@
-var bounce=document.getElementById("bounce-mot"),x=0,directionX=!0;function bouncing(){directionX?(650<=x&&(directionX=!1),x+=1):directionX||(x<=-10&&(directionX=!0),x-=1),bounce.style.left=x+"px"}var setting=setInterval(bouncing,70);
+var bounce = document.getElementById("bounce-mot"),
+  x = 0,
+  directionX = !0;
+function bouncing(y) {
+  var count = 650;
+  if (y.matches) {
+    count = 300;
+  } else {
+    count = 650;
+  }
+  directionX
+    ? (count <= x && (directionX = !1), (x += 1))
+    : directionX || (x <= -10 && (directionX = !0), (x -= 1)),
+    (bounce.style.left = x + "px");
+}
+var setting = setInterval(function() {
+  bouncing(y);
+}, 70);
+
+var y = window.matchMedia("(max-width: 500px)");
+// Call listener function at run time
+y.addListener(bouncing); // Attach listener function on state changes
